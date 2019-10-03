214807d06bafb51d9b4233cab3644e90fccc7f25
Steven Huang <steven.charles.huang@gmail.com>
2019-10-02 20:06:52 -0400

    Completed basic requirements of the assignment

--- starter/Animal.java
+++ starter/Animal.java
@@ -6,7 +6,46 @@ public class Animal {
 
     public Animal(int numLegs, int topSpeed, boolean isEndangered, String name){
         //Set properties here
+        this.numLegs = numLegs;
+        this.topSpeed = topSpeed;
+        this.isEndangered = isEndangered;
+        this.name = name;
     }
 
     //Put getters and setters here
+    public int getNumLegs() {
+        return numLegs;
+    }
+
+    public void setNumLegs(int numLegs) {
+        this.numLegs = numLegs;
+    }
+
+    public int getTopSpeed() {
+        return topSpeed;
+    }
+
+    public void setTopSpeed(int topSpeed) {
+        this.topSpeed = topSpeed;
+    }
+
+    public boolean isEndangered() {
+        return isEndangered;
+    }
+
+    public void setEndangered(boolean endangered) {
+        isEndangered = endangered;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFact() {
+        return "The " + name + " has a top speed of " + topSpeed + " mph.";
+    }
 }
--- starter/Main.java
+++ starter/Main.java
@@ -5,25 +5,32 @@ public class Main {
     public static void main(String[] args) {
 
         //Instantiate new Animal
-        Animal animal = ;
+        Animal animal = new Animal(4, 10, false, "Penguin");
 
         String name;
         int topSpeed;
 
         //get name and speed values using getters
-        name = ;
-        topSpeed = ;
+        name = animal.getName();
+        topSpeed = animal.getTopSpeed();
 
         //Print some output
+        System.out.println(name);
+        System.out.println(topSpeed);
 
         //Set new name, speed, and endangered properties values using setters
-
+        animal.setName("Amur Leopard");
+        animal.setTopSpeed(37);
+        animal.setEndangered(true);
 
         //get new values using getters
-        name = ;
-        topSpeed = ;
+        name = animal.getName();
+        topSpeed = animal.getTopSpeed();
 
         //Print some output
-        
+        System.out.println(name);
+        System.out.println(topSpeed);
+
+        System.out.println(animal.getFact());
     }
 }
